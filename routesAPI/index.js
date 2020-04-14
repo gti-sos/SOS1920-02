f764de33c23d2e0758736837cb5536ba8e918a9b
anafraire <anafragon@alum.us.es>
2020-04-14 17:01:18 +0000

    busqueda

--- routesAPI/index.js
+++ routesAPI/index.js
@@ -17,7 +17,7 @@ module.exports = function(app){
 	
 	var initialRoutes = [
 	{province: "almeria", year: 2015, metropolitan: 77.6, urban: 53.2, rest: 24.3},
-	{province: "cadiz",	year: 2015,	metropolitan: 195.6, urban: 9.6, rest: 2.8},
+	{province: "cadiz", year: 2015, metropolitan: 195.6, urban: 9.6, rest: 2.8},
 	{province: "cordoba", year: 2015, metropolitan: 10.9, urban: 59.5, rest: 6.1},
 	{province: "granada", year: 2015, metropolitan: 47.7, urban: 24.8,rest: 13.6},
 	{province: "huelva", year: 2015, metropolitan: 116, urban: 15.7,rest: 2.1},
@@ -38,33 +38,60 @@ app.get(BASE_API_URL+"/evolution-of-cycling-routes/loadInitialData", (req,res) =
 });
 
 
-// GET /evolution-of-cycling-routes?limit=8&offset=0
-	
+	// GET /evolution-of-cycling-routes?limit=8&offset=0
+
 app.get(BASE_API_URL+"/evolution-of-cycling-routes", (req,res) =>{
 	var query = {};
 	let offset = 0;
 	let limit = Number.MAX_SAFE_INTEGER;
-		
-//PAGINACIÓN
-        if (req.query.offset) {
-            offset = parseInt(req.query.offset);
-            delete req.query.offset;
-        }
-        if (req.query.limit) {
-            limit = parseInt(req.query.limit);
-            delete req.query.limit;
-        }		
-		db.find({}).sort({province:1,year:-1}).skip(offset).limit(limit).exec((error, routes) =>{
-			console.log("valor del offset: " +offset);
-			console.log("valor del limit: " +limit);
-			routes.forEach((r)=>{
-				delete r._id
-			});
-
-			res.send(JSON.stringify(routes,null,2));
-			console.log("Recursos mostrados");
+	console.log("limit= "+ limit+", offset= "+ offset);
+	
+	var province = req.query.province;
+	var year = parseInt(req.query.year);
+	var metropolitan = parseInt(req.query.metropolitan);
+	var urban = parseInt(req.query.urban);
+	var rest = parseInt(req.query.rest);
+	
+	db.find(dbquery).sort({province:1,year:-1}).skip(offset).limit(limit).exec((error, routes) => {
+		routes.forEach((v) => {
+			delete v._id;
 		});
+		
+		if(province){
+			routes = routes.filter(function(route) {
+ 				return route.province == province;
+			});
+		};
+		
+		if(year){
+			routes = routes.filter(function(route) {
+ 				return route.year == year;
+			});
+		};
+		
+		if(metropolitan){
+			routes = routes.filter(function(route) {
+ 				return route.year == metropolitan;
+			});
+		};	
+		
+		if(urban){
+			routes = routes.filter(function(route) {
+ 				return route.year == urban;
+			});
+		};
+		
+		if(rest){
+			routes = routes.filter(function(route) {
+ 				return route.year == rest;
+			});
+		};
+		
+		res.send(JSON.stringify(routes,null,2));
+		console.log("Data sent:"+JSON.stringify(routes,null,2));
+
 	});
+});
 	
 // GET /evolution-of-cycling-routes/XXX/YYY
 	
