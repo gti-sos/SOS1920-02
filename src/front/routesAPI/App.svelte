292ade7c3d5adfef58802eacbaef6a992b8593d8
anafraire <anafragon@alum.us.es>
2020-05-27 01:21:50 +0200

    PRUEBA ANA

--- src/front/routesAPI/App.svelte
+++ src/front/routesAPI/App.svelte
@@ -2,7 +2,6 @@
 import CyclingRoutesTable from './CyclingRoutesTable.svelte';
 </script>
 
-
 <main>
 	<h1 class="display-4" style="text-align: center;" >Evolucion de carriles bici</h1>
 	<CyclingRoutesTable/>			
