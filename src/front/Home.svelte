8386baf9f19b5d597f97b853f3641ee0c78e383d
Jose Francisco Morales Ruiz <josmorrui2@alum.us.es>
2020-05-26 19:36:30 +0200

    diseño

--- src/front/Home.svelte
+++ src/front/Home.svelte
@@ -2,7 +2,12 @@
 	<div class="div-home">
 
 		<div style="margin-left: 12.5%;">
-			<br>
+			<br><br>
+
+			<button type="button" class="btn btn-secondary" onclick="window.location.href='#/evolution-of-cycling-routes'" style="margin-left: 6.75%; width: 25%;"> API Evolucion Carriles Bici</button>
+			<button type="button" class="btn btn-secondary" onclick="window.location.href='#/rural-tourism-stats'" style="width: 25%;"> API Turismo Rural</button>
+			<button type="button" class="btn btn-secondary" onclick="window.location.href='#/traffic-accidents'" style="width: 25%;"> API Accidentes De Tráfico </button><br><br>
+
 			<ul>
 				<li><strong>Team: </strong></li>
 				<ul>
@@ -17,16 +22,10 @@
 				<li><strong>URL AZURE: </strong><a href="https://sos1920-02.azurewebsites.net/">https://sos1920-02.azurewebsites.net/</a></li>
 				<li><strong>APIs: </strong></li>
 				<ul>
-					<li><a href="https://documenter.getpostman.com/view/10637431/SzezcrsS">https://sos1920-02.herokuapp.com/api/v1/evolution-of-cycling-routes</a> (developed by <a href="https://github.com/anafraire">Ana
-						Fraire Gonzalez</a>)</li>
 					<li><a href="https://documenter.getpostman.com/view/10637431/Szmcaz3h">https://sos1920-02.herokuapp.com/api/v2/evolution-of-cycling-routes</a> (developed by <a href="https://github.com/anafraire">Ana
 						Fraire Gonzalez</a>)</li>
-					<li><a href="https://documenter.getpostman.com/view/10653459/SzYT6hqm">https://sos1920-02.herokuapp.com/api/v1/traffic-accidents</a> (developed by <a href="https://github.com/josmorrui2">Jose
-						Francisco Morales Ruiz</a>)</li>
 					<li><a href="https://documenter.getpostman.com/view/10653459/SzmcbKXc">https://sos1920-02.herokuapp.com/api/v2/traffic-accidents</a> (developed by <a href="https://github.com/josmorrui2">Jose
 						Francisco Morales Ruiz</a>)</li>
-					<li><a href="https://documenter.getpostman.com/view/6870023/SzYZ2KVA">https://sos1920-02.herokuapp.com/api/v1/rural-tourism-stats</a> (developed by <a href="https://github.com/martaverdugo6">
-						Marta Verdugo Martinez</a>)</li>
 					<li><a href="https://documenter.getpostman.com/view/6870023/SzmcbKTA">https://sos1920-02.herokuapp.com/api/v2/rural-tourism-stats</a> (developed by <a href="https://github.com/martaverdugo6">
 						Marta Verdugo Martinez</a>)</li>
 				</ul>
@@ -35,9 +34,7 @@
 
 			</ul>
 		<br>
-		<button type="button" class="btn btn-primary" onclick="window.location.href='#/evolution-of-cycling-routes'" style="margin-left: 6.75%; width: 25%;"> API Evolucion Carriles Bici</button>
-		<button type="button" class="btn btn-warning" onclick="window.location.href='#/rural-tourism-stats'" style="width: 25%;"> API Turismo Rural</button>
-		<button type="button" class="btn btn-danger" onclick="window.location.href='#/traffic-accidents'" style="width: 25%;"> API Accidentes De Tráfico </button><br><br>
+		
 		<button type="button" class="btn btn-primary" onclick="window.location.href='#/evolution-of-cycling-routes/graph'" style="margin-left: 6.75%; width: 25%;"> Estadísticas Evolucion Carriles Bici</button>
 		<button type="button" class="btn btn-warning" onclick="window.location.href='#/rural-tourism-stats/graph'" style="width: 25%;"> Estadísticas Turismo Rural</button>
 		<button type="button" class="btn btn-danger" onclick="window.location.href='#/traffic-accidents/graph'" style="width: 25%;">Estadísticas Accidentes Tráfico</button><br><br>
