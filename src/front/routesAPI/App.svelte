0dd897f410878e41198433218db087f4ed71f882
anafraire <anafragon@alum.us.es>
2020-05-26 21:42:12 +0200

    probando el repositorio 5

--- src/front/routesAPI/App.svelte
+++ src/front/routesAPI/App.svelte
@@ -1,6 +1,7 @@
 <script>
 import CyclingRoutesTable from './CyclingRoutesTable.svelte';
 </script>
+
 <main>
 	<h1 class="display-4" style="text-align: center;" >Evolucion de carriles bici</h1>
 	<CyclingRoutesTable/>			
