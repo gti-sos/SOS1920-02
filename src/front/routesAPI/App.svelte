eb8b60940da50a017c5c57164f2dd137eb458122
anafraire <anafragon@alum.us.es>
2020-05-26 21:18:18 +0200

    problemas con el repositorio

--- src/front/routesAPI/App.svelte
+++ src/front/routesAPI/App.svelte
@@ -4,5 +4,6 @@ import CyclingRoutesTable from './CyclingRoutesTable.svelte';
 
 <main>
 	<h1 class="display-4" style="text-align: center;" >Evolucion de carriles bici</h1>
-	<CyclingRoutesTable/>			
+	<CyclingRoutesTable/>	
+			
 </main>
