76eb8df92af35cdb1709533db4c36f45d10f1647
Jose Francisco Morales Ruiz <josmorrui2@alum.us.es>
2020-05-27 20:47:39 +0200

    Azure prueba 2

--- src/front/Home.svelte
+++ src/front/Home.svelte
@@ -1,50 +1,7 @@
 <main>
 	<div class="div-home">
 
-		<div style="margin-left: 12.5%;">
-			<br><br>
-
-			<button type="button" class="btn btn-secondary" onclick="window.location.href='#/evolution-of-cycling-routes'" style="margin-left: 6.75%; width: 25%;"> API Evolucion Carriles Bici</button>
-			<button type="button" class="btn btn-secondary" onclick="window.location.href='#/rural-tourism-stats'" style="width: 25%;"> API Turismo Rural</button>
-			<button type="button" class="btn btn-secondary" onclick="window.location.href='#/traffic-accidents'" style="width: 25%;"> API Accidentes De Tráfico </button><br><br>
-
-			<ul>
-				<li><strong>Team: </strong></li>
-				<ul>
-					<li><a href="https://github.com/anafraire">Ana Fraire González</a></li>
-					<li><a href="https://github.com/josmorrui2">José Francisco Morales Ruiz</a></li>
-					<li><a href="https://github.com/martaverdugo6">Marta Verdugo Martínez</a></li>
-				</ul>
-				<li><strong>Project description: </strong>Analysis of the relationship between the traffic accidents, rural tourism and evolution of
-					cycling routes</li>	
-				<li><strong>Repository: </strong><a href="https://github.com/gti-sos/SOS1920-02">gti-sos/SOS1920-02</a></li>
-				<li><strong>URL HEROKU: </strong><a href="http://sos1920-02.herokuapp.com">http://sos1920-02.herokuapp.com</a></li>
-				<li><strong>URL AZURE: </strong><a href="https://sos1920-02.azurewebsites.net/">https://sos1920-02.azurewebsites.net/</a></li>
-				<li><strong>APIs: </strong></li>
-				<ul>
-					<li><a href="https://documenter.getpostman.com/view/10637431/Szmcaz3h">https://sos1920-02.herokuapp.com/api/v2/evolution-of-cycling-routes</a> (developed by <a href="https://github.com/anafraire">Ana
-						Fraire Gonzalez</a>)</li>
-					<li><a href="https://documenter.getpostman.com/view/10653459/SzmcbKXc">https://sos1920-02.herokuapp.com/api/v2/traffic-accidents</a> (developed by <a href="https://github.com/josmorrui2">Jose
-						Francisco Morales Ruiz</a>)</li>
-					<li><a href="https://documenter.getpostman.com/view/6870023/SzmcbKTA">https://sos1920-02.herokuapp.com/api/v2/rural-tourism-stats</a> (developed by <a href="https://github.com/martaverdugo6">
-						Marta Verdugo Martinez</a>)</li>
-				</ul>
-
-
-
-			</ul>
-		<br>
-		
-		<button type="button" class="btn btn-primary" onclick="window.location.href='#/evolution-of-cycling-routes/graph'" style="margin-left: 6.75%; width: 25%;"> Estadísticas Evolucion Carriles Bici</button>
-		<button type="button" class="btn btn-warning" onclick="window.location.href='#/rural-tourism-stats/graph'" style="width: 25%;"> Estadísticas Turismo Rural</button>
-		<button type="button" class="btn btn-danger" onclick="window.location.href='#/traffic-accidents/graph'" style="width: 25%;">Estadísticas Accidentes Tráfico</button><br><br>
-		<button type="button" class="btn btn-primary" onclick="window.location.href='#/evolution-of-cycling-routes/graph-v2'" style="margin-left: 6.75%; width: 25%;"> Estadísticas Evolucion Carriles Bici</button>
-		<button type="button" class="btn btn-warning" onclick="window.location.href='#/rural-tourism-stats/graph-v2'" style="width: 25%;"> Estadísticas Turismo Rural</button>
-		<button type="button" class="btn btn-danger" onclick="window.location.href='#/traffic-accidents/graph-v2'" style="width: 25%;">Gráfica Accidentes</button><br><br>
-		<button type="button" class="btn btn-secondary" onclick="window.location.href='#/analytics'" style="margin-left: 6.75%; width: 25%;">Analysis</button>
-		<button type="button" class="btn btn-secondary" onclick="window.location.href='#/integrations'" style="width: 25%;">Integraciones</button>
-		<br>
-		</div>
+		<h2>hola</h2>
 		
 	</div>
 </main>
