b5fdc0d2be04f2a9452f88ed82d7e5a8395ad1d7
Marta Verdugo <marta_v_m6@hotmail.com>
2020-05-29 14:14:39 +0000

    estructura integracion touris

--- src/front/Integrations.svelte
+++ src/front/Integrations.svelte
@@ -9,8 +9,8 @@
     <br><br>   
 
     <button type="button" class="btn btn-secondary" onclick="window.location.href='#/integrations/cycling-routes'" style="margin-left: 12.5%; width: 25%;"> Evolucion Carriles Bici</button>
-	<button type="button" class="btn btn-secondary" onclick="window.location.href='#/rural-tourism-stats'" style="width: 25%;">Turismo Rural</button>
-	<button type="button" class="btn btn-secondary" onclick="window.location.href='#/integrations/accidents'" style= "width: 25%;">Accidentes De Tráfico </button><br><br>
+	<button type="button" class="btn btn-secondary" onclick="window.location.href='#/integrations/rural-tourism-stats'" style="width: 25%;">Turismo Rural</button>
+	<button type="button" class="btn btn-secondary" onclick="window.location.href='#/integrations/accidents'" style= "width: 25%;">Accidentes De Trรกfico </button><br><br>
 
     <Button outline color="secondary" on:click="{pop}">Volver</Button>
 
