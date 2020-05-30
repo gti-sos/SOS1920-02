ee6134d3e221348396df6ff7f9890c051ea4b116
Jose Francisco Morales Ruiz <josmorrui2@alum.us.es>
2020-05-30 20:17:46 +0200

    API external Ana

--- src/front/routesAPI/Integrations/apiExtena01.svelte
+++ src/front/routesAPI/Integrations/apiExtena01.svelte
@@ -17,37 +17,100 @@
             apiExterna = json;
 		} else {
 			console.log("ERROR!");
-        } 
+        }
+        let frase = apiExterna.value;
+    
+        var text = frase;
+        var lines = text.split(/[,\. ]+/g),
+        data = Highcharts.reduce(lines, function (arr, word) {
+            var obj = Highcharts.find(arr, function (obj) {
+            return obj.name === word;
+            });
+            if (obj) {
+            obj.weight += 1;
+            } else {
+            obj = {
+                name: word,
+                weight: 1
+            };
+            arr.push(obj);
+            }
+            return arr;
+        }, []);
+
+        Highcharts.chart('container', {
+        accessibility: {
+            screenReaderSection: {
+            beforeChartFormat: '<h5>{chartTitle}</h5>' +
+                '<div>{chartSubtitle}</div>' +
+                '<div>{chartLongdesc}</div>' +
+                '<div>{viewTableButton}</div>'
+            }
+            },
+            series: [{
+                type: 'wordcloud',
+                data: data,
+                name: 'Occurrences'
+            }],
+            title: {
+                text: 'Frases de Chucknorris'
+            }
+        });
+        
     }
-    onMount(loadGraph);
 
 </script>
 
-<main>
-    
-    <div class="container" style="background-color: rgb(46, 49, 49);padding:1% 1% 1% 1%; border-radius: 20px;box-shadow: 0px 0px 16px -2px rgba(0,0,0,0.52);">
-        <div style="padding-bottom:2%;">
-            
-            <div class="table-responsive-xl" style="background-color:white;">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col">
-                                Historia real de Chuck Norris:
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>
-                                {apiExterna.value}
-                            </td>
-                        </tr>
-                    </tbody><br>
-                    <button type="button" class="btn btn-secondary" onclick="location.reload()" style="margin-left: 35%; width: 25%;"> Siguiente frase</button><br><br>
-                </table>
-            </div>
-        </div>
-    </div>
+<svelte:head>
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/wordcloud.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js" on:load="{loadGraph}"></script>
+</svelte:head>
 
+<main>
+    <figure class="highcharts-figure">
+        <div id="container"></div>
+        <p class="highcharts-description">
+          {apiExterna.value}
+        </p>
+    </figure>
+    <button type="button" class="btn btn-secondary" onclick="location.reload()" style="margin-left: 35%; width: 25%;"> Siguiente frase</button>
 </main>
+
+<style>
+    .highcharts-figure, .highcharts-data-table table {
+        min-width: 320px; 
+        max-width: 800px;
+        margin: 1em auto;
+    }
+
+    .highcharts-data-table table {
+            font-family: Verdana, sans-serif;
+            border-collapse: collapse;
+            border: 1px solid #EBEBEB;
+            margin: 10px auto;
+            text-align: center;
+            width: 100%;
+            max-width: 500px;
+    }
+    .highcharts-data-table caption {
+        padding: 1em 0;
+        font-size: 1.2em;
+        color: #555;
+    }
+    .highcharts-data-table th {
+        font-weight: 600;
+        padding: 0.5em;
+    }
+    .highcharts-data-table td, .highcharts-data-table th, .highcharts-data-table caption {
+        padding: 0.5em;
+    }
+    .highcharts-data-table thead tr, .highcharts-data-table tr:nth-child(even) {
+        background: #f8f8f8;
+    }
+    .highcharts-data-table tr:hover {
+        background: #f1f7ff;
+    }
+</style>
